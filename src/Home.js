cbe1a99f78a6c1c32a66f16ea02ac3cc46bbc1b6
mode-x <abiaemma@gmail.com>
2022-06-02 11:36:16 +0100

    refactor: Improve Books component

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import Books from "./Books";
 
 const Home = ({ books }) => {
+  const shelves = [
+    { id: "currentlyReading", name: "Currently Reading" },
+    { id: "wantToRead", name: "Want to Read" },
+    { id: "read", name: "Read" },
+  ];
   const booksInShelf = (shelf) => {
     return books.filter((book) => book.shelf === shelf);
   };
@@ -12,26 +17,14 @@ const Home = ({ books }) => {
         <h1>MyReads</h1>
       </div>
       <div className="list-books-content">
-        <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
+        {shelves.map((shelf) => (
+          <div className="bookshelf" key={shelf.id}>
+            <h2 className="bookshelf-title">{shelf.name}</h2>
             <div className="bookshelf-books">
-              <Books books={booksInShelf("currentlyReading")} />
+              <Books books={booksInShelf(shelf.id)} />
             </div>
           </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <Books books={booksInShelf("wantToRead")} />
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <Books books={booksInShelf("read")} />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="open-search">
         <Link to="/search">Add a book</Link>
